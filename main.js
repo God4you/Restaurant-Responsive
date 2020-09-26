b682e22dbafe2bd1a9fcfba4de4819aac2c82dfd
Benjamin Kakar <j0h@example.com>
2020-09-26 12:07:27 +0200

    only media queries is left.

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const selectElement = function (element) {
 
 //selecting the class that we want to select with the funcation we created and storred in variables.
 let menuToggler = selectElement(".menu-toggle");
-let body = selectElement("body");
+//let body = document.getElementsByTagName("body")[0];
+//let body = selectElement("body");
+let body = document.body;
 
-menuToggler.addEventlistener("click", function () {
+menuToggler.addEventListener("click", function () {
   body.classList.toggle("open");
 });
